dd974ad5b54db4fd450c60c0b5fc536ba5f13645
johnhcc <8312113+johnhcc@users.noreply.github.com>
2025-04-18 22:40:50 -0600

    Create ghostscript-x11.rb

---
+++ Formula/ghostscript-x11.rb
@@ -0,0 +1,98 @@
+class GhostscriptX11 < Formula
+  desc "Interpreter for PostScript and PDF"
+  homepage "https://www.ghostscript.com/"
+  url "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs10050/ghostpdl-10.05.0.tar.xz"
+  sha256 "f154039345b6e9957b0750f872374d887d76321d52bbcc9d3b85487855e08f02"
+  license "AGPL-3.0-or-later"
+  revision 1
+
+  # The GitHub tags omit delimiters (e.g. `gs9533` for version 9.53.3). The
+  # `head` repository tags are formatted fine (e.g. `ghostpdl-9.53.3`) but a
+  # version may be tagged before the release is available on GitHub, so we
+  # check the version from the first-party website instead.
+  livecheck do
+    url "https://www.ghostscript.com/json/settings.json"
+    regex(/["']GS_VER["']:\s*?["']v?(\d+(?:\.\d+)+)["']/i)
+  end
+
+  keg_only :shadowed_by_ghostscript, "ghostscript core formula provides non-X11 version"
+
+  head do
+    url "https://git.ghostscript.com/ghostpdl.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "jbig2dec"
+  depends_on "jpeg-turbo"
+  depends_on "leptonica"
+  depends_on "libarchive"
+  depends_on "libidn"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "libx11"
+  depends_on "libxt"
+  depends_on "little-cms2"
+  depends_on "openjpeg"
+  depends_on "tesseract"
+
+  uses_from_macos "expat"
+  uses_from_macos "zlib"
+
+  conflicts_with "gambit-scheme", because: "both install `gsc` binary"
+  conflicts_with "git-spice", because: "both install `gs` binary"
+
+  # https://sourceforge.net/projects/gs-fonts/
+  resource "fonts" do
+    url "https://downloads.sourceforge.net/project/gs-fonts/gs-fonts/8.11%20%28base%2035%2C%20GPL%29/ghostscript-fonts-std-8.11.tar.gz"
+    sha256 "0eb6f356119f2e49b2563210852e17f57f9dcc5755f350a69a46a0d641a0c401"
+  end
+
+  def install
+    # Delete local vendored sources so build uses system dependencies
+    libs = %w[expat freetype jbig2dec jpeg lcms2mt leptonica libpng openjpeg tesseract tiff zlib]
+    libs.each { |l| rm_r(buildpath/l) }
+
+    configure = build.head? ? "./autogen.sh" : "./configure"
+
+    args = %w[--disable-compile-inits
+              --disable-cups
+              --disable-gtk
+              --with-system-libtiff
+              --without-versioned-path]
+
+    # Set the correct library install names so that `brew` doesn't need to fix them up later.
+    ENV["DARWIN_LDFLAGS_SO_PREFIX"] = "#{opt_lib}/"
+    system configure, *args, *std_configure_args
+
+    # Install binaries and libraries
+    system "make", "install"
+    ENV.deparallelize { system "make", "install-so" } unless OS.mac?
+
+    (pkgshare/"fonts").install resource("fonts")
+
+    # Temporary backwards compatibility symlinks
+    if build.stable?
+      odie "Remove backwards compatibility symlink and caveat!" if version >= "10.07"
+      pkgshare.install_symlink pkgshare => version.to_s
+      doc.install_symlink doc => version.to_s
+    end
+  end
+
+  def caveats
+    <<~CAVEATS
+      Ghostscript is now built `--without-versioned-path`. Temporary backwards
+      compatibility symlinks exist but will be removed with 10.07.0 release.
+    CAVEATS
+  end
+
+  test do
+    ps = test_fixtures("test.ps")
+    assert_match "Hello World!", shell_output("#{bin}/ps2ascii #{ps}")
+  end
+end
