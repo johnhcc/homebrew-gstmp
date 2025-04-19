98a4fabcaf39027b8d58c6dcca59bf039182001b
johnhcc <8312113+johnhcc@users.noreply.github.com>
2025-04-18 23:33:33 -0600

    Update ghostscript-x11.rb

--- Formula/ghostscript-x11.rb
+++ Formula/ghostscript-x11.rb
@@ -39,7 +39,7 @@ class GhostscriptX11 < Formula
   depends_on "libxt"
   depends_on "little-cms2"
   depends_on "openjpeg"
-  # depends_on "openlibm"
+  depends_on "openlibm"
   depends_on "tesseract"
 
   uses_from_macos "expat"
